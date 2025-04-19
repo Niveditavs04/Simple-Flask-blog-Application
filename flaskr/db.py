6715d9c58d9830360131aa948af8f85f46946eeb
Nivedita.vs <141635381+Niveditavs04@users.noreply.github.com>
2025-04-19 13:08:55 +0530

    Update db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -34,30 +34,9 @@ def close_db(e=None):
 def init_db():
     db = get_db()
 
-    # Create tables if they don't exist
-    
-    db.executescript("""
-    DROP TABLE IF EXISTS user;
-    DROP TABLE IF EXISTS post;
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
 
-    CREATE TABLE user (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-    );
-
-    CREATE TABLE post (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        author_id INTEGER NOT NULL,
-        created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        title TEXT NOT NULL,
-        body TEXT NOT NULL,
-        FOREIGN KEY (author_id) REFERENCES user (id)
-          );
-             """)
-    db.commit()
-
-    
 
 @click.command('init-db')
 def init_db_command():
